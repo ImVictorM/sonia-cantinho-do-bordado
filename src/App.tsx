2e506e1ee87ebc58cb807c74d0f587550928edbf
Victor Figueiredo Mendes <victorutdsim7@gmail.com>
2026-05-21 17:58:51 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="container bg-background">
+      <div className="flex flex-col p-4">
+        <h1 className="text-primary text-2xl font-semibold">
+          Template React TS Tailwind Vite
+        </h1>
+        <ul className="list-disc list-inside mt-4 text-foreground text-lg">
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>Tailwind CSS</li>
+          <li>Vite</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
